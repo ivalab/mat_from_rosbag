08137fa94cadd210de619b060ee515556877e63d
Yanwei Du <yanwei.du@gatech.edu>
2023-02-15 00:34:57 -0500

    Update scripts to evaluate both navigation and estimation errors

--- script/batch_collect.py
+++ script/batch_collect.py
@@ -6,28 +6,30 @@ from pathlib import Path
 import subprocess
 import zipfile
 
-SeqNameList = ["loop", "long", "square", "zigzag", "two_circle", "infinite"]
-SeqNameToTableIndex = ["s1", "s2", "m1", "m2", "l1", "l2"]
-SeqLenList = [40, 50, 105, 125, 200, 245]
-RESULT_ROOT = "/mnt/DATA/Yanwei/closedloop/2023/20.04/"
-ET_FREQ = 100.0
-
 ## adjust the variables
+RESULT_ROOT = "/mnt/DATA/Yanwei/closedloop/2023/20.04/"
 ImuPool = ["ADIS16448", "mpu6000"]
 VelPool = [0.5, 1.0, 1.5]
 NumRepeating = 3
-Number_GF_List = [20]  # [40, 60, 80, 120, 160];
-MethodPool = ["DSOL"]  # ['GF_GG_skf'] # [ORB3]
+Number_GF_List = [400, 800]  # [40, 60, 80, 120, 160];
+MethodPool = ["ORB3"]  # ['GF_GG_skf'] # [ORB3]
 
-## evaluation threshold
+# dataset variables
+SeqNameList = ["loop", "long", "square", "zigzag", "two_circle", "infinite"]
+SeqNameToTableIndex = ["s1", "s2", "m1", "m2", "l1", "l2"]
+SeqLenList = [40, 50, 105, 125, 200, 245]
+
+## evaluation parameters
+ResultFilePool = ["nav", "est"]
 RMSE_THRESH = 10.0  # m
 TRACKING_THRESH = 0.6
+ET_FREQ = 100.0
 
 for method in MethodPool:
 
     for feature in Number_GF_List:
 
-        rmse_table = np.full((len(ImuPool), len(VelPool), len(SeqNameList), NumRepeating), -1.0)
+        rmse_table = np.full((len(ResultFilePool), len(ImuPool), len(VelPool), len(SeqNameList), NumRepeating), -1.0)
 
         for imu_index, imu_type in enumerate(ImuPool):
 
@@ -46,21 +48,22 @@ for method in MethodPool:
                         # print(prefix)
                         # print(os.path.exists(prefix + '.bag'))
 
-                        file_result = prefix + "_result.zip"
-                        if not os.path.exists(file_result):
-                            print(f"{file_result} not exists")
-                            continue
+                        for file_index, file_type in enumerate(ResultFilePool):
+                            file_result = prefix + "_result_" + file_type + ".zip"
+                            if not os.path.exists(file_result):
+                                print(f"{file_result} not exists")
+                                continue
 
-                        with zipfile.ZipFile(file_result, "r") as z:
-                            with z.open("stats.json") as f:
-                                data = f.read()
-                                error = json.loads(data)["rmse"]
-                                if error < RMSE_THRESH:
-                                    rmse_table[imu_index, vel_index, seq_index, round_index] = error
-                                else:
-                                    print(f"{file_result} large rmse: {error}")
-                                if error > 1.0:
-                                    print(f"{file_result} large rmse: {error}")
+                            with zipfile.ZipFile(file_result, "r") as z:
+                                with z.open("stats.json") as f:
+                                    data = f.read()
+                                    error = json.loads(data)["rmse"]
+                                    if error < RMSE_THRESH:
+                                        rmse_table[file_index, imu_index, vel_index, seq_index, round_index] = error
+                                    else:
+                                        print(f"{file_result} large rmse: {error}")
+                                    # if error > 1.0:
+                                    # print(f"{file_result} large rmse: {error}")
 
                         # check tracking ratio
                         file_plan = prefix + "_arr_plan.txt"
@@ -71,32 +74,35 @@ for method in MethodPool:
                         acc_duration = array_et[-1, 0] - array_et[0, 0]
                         if acc_duration < exp_duration * TRACKING_THRESH:
                             print(f"tracking failed: {file_et}, tracking ratio: {acc_duration}")
-                            rmse_table[imu_index, vel_index, seq_index, round_index] = -1.0
+                            for file_index in range(ResultFilePool):
+                                rmse_table[file_index, imu_index, vel_index, seq_index, round_index] = -1.0
 
-        cur_table = np.full((len(ImuPool) * len(VelPool), len(SeqNameList) + 1), -1.0)
-        for i in range(rmse_table.shape[0]):
-            for j in range(rmse_table.shape[1]):
-                ridx = i * len(VelPool) + j
-                for k in range(rmse_table.shape[2]):
-                    indices = rmse_table[i, j, k, :] > 0.0
-                    if np.sum(indices) == NumRepeating:
-                        cur_table[ridx, k] = np.mean(rmse_table[i, j, k, :], axis=-1)
-                indices = cur_table[ridx, :] > 0.0
-                if np.sum(indices) == 0:
-                    continue
-                cur_table[ridx, -1] = np.mean(cur_table[ridx, indices])
-        print(cur_table)
-        np.savetxt(
-            os.path.join(RESULT_ROOT, method + "_" + str(feature) + "_closedloop_vis.txt"),
-            cur_table,
-            fmt="%.6f",
-            delimiter=",",
-            header=" ".join(SeqNameList) + " mean ",
-        )
-        np.savetxt(
-            os.path.join(RESULT_ROOT, method + "_" + str(feature) + "_closedloop.txt"),
-            rmse_table.reshape(len(ImuPool) * len(VelPool) * len(SeqNameList), -1),
-            fmt="%.6f",
-            delimiter=",",
-            header=" ".join(["Round" + str(i) for i in range(1, NumRepeating + 1)]),
-        )
+        for file_index, file_type in enumerate(ResultFilePool):
+            cur_table = np.full((len(ImuPool) * len(VelPool), len(SeqNameList) + 1), -1.0)
+            for i in range(rmse_table.shape[1]):
+                for j in range(rmse_table.shape[2]):
+                    ridx = i * len(VelPool) + j
+                    for k in range(rmse_table.shape[3]):
+                        indices = rmse_table[file_index, i, j, k, :] > 0.0
+                        if np.sum(indices) == NumRepeating:
+                            cur_table[ridx, k] = np.mean(rmse_table[file_index, i, j, k, :], axis=-1)
+                    indices = cur_table[ridx, :] > 0.0
+                    if np.sum(indices) == 0:
+                        continue
+                    cur_table[ridx, -1] = np.mean(cur_table[ridx, indices])
+            print(cur_table)
+            file_name = "_closedloop_" + file_type
+            np.savetxt(
+                os.path.join(RESULT_ROOT, method + "_" + str(feature) + file_name + "_vis.txt"),
+                cur_table,
+                fmt="%.6f",
+                delimiter=",",
+                header=" ".join(SeqNameList) + " mean ",
+            )
+            np.savetxt(
+                os.path.join(RESULT_ROOT, method + "_" + str(feature) + file_name + ".txt"),
+                rmse_table[file_index, :, :, :, :].reshape(len(ImuPool) * len(VelPool) * len(SeqNameList), -1),
+                fmt="%.6f",
+                delimiter=",",
+                header=" ".join(["Round" + str(i) for i in range(1, NumRepeating + 1)]),
+            )
--- script/batch_evaluate.py
+++ script/batch_evaluate.py
@@ -6,32 +6,91 @@ from pathlib import Path
 import subprocess
 import zipfile
 
-CLOSEDLOOP_DIR = '/mnt/DATA/Yanwei/closedloop/2023/20.04/'
-BAGFILES = Path(CLOSEDLOOP_DIR).rglob('*.bag')
-# IMUS = ['mpu6000']
-IMUS = ['mpu6000', 'ADIS16448']
-METHODS = ['DSOL']
+
+## Please configure path
+CLOSEDLOOP_DIR = "/mnt/DATA/Yanwei/closedloop/2023/20.04/"
+IMUS = ["mpu6000", "ADIS16448"]
+METHODS = ["ORB3"]
 
 enable_evaluation = True
 
-# -------------------------------------------------------------------------------
+## Please keep the following values unchanged.
+BAGFILES = Path(CLOSEDLOOP_DIR).rglob("*.bag")
+enable_planning_time_compenstation = False
+desired_path_topic = "/desired_path_compensated"  # "/desired_path"
+
+# ------------------------------------------------------------------------------
+def compensate_planning_time(plan_txyz, act_txyz):
+    """
+    In current implementation, /desired_path from planning module is stamped
+    with the time(t0) when triggered, however controller module assigned the
+    /desired_path with the latest odometry timestamp(t1). Because a very large
+    planning time causes delay and leads the robot to track a very far-away
+    waypoint. This function approximates the offset (t0 - t1) by measuring the
+    change in act and plan files.
+    """
+
+    assert plan_txyz.shape[0] > 10
+    assert plan_txyz.shape[1] == 4
+    assert act_txyz.shape[0] > 10
+    assert act_txyz.shape[1] == 4
+
+    act_idx = 0
+    act_timestamps = []
+    plan_timestamps = []
+    plan_idx = 0
+    for dist in np.arange(0.1, 1.1, 0.1):
+        while np.linalg.norm(act_txyz[act_idx, 1:]) < dist:
+            act_idx += 1
+        act_timestamps.append(act_txyz[act_idx, 0])
+        while np.linalg.norm(plan_txyz[plan_idx, 1:]) < dist:
+            plan_idx += 1
+        plan_timestamps.append(plan_txyz[plan_idx, 0])
+
+    timeoffset = np.mean(np.array(plan_timestamps) - np.array(act_timestamps))
+
+    return timeoffset if np.abs(timeoffset) < 10.0 else np.nan
 
 
-def evo_evaluate(prefix, name):
-    basename = name.split('.')[0]
-    file_gt = os.path.join(prefix, basename + '_arr_act.txt')
-    file_et = os.path.join(prefix, basename + '_arr_est.txt')
-    file_result = os.path.join(prefix, basename + '_result.zip')
-    if os.path.exists(file_result):
-        cmd_rm = 'rm ' + file_result
+def evo_evaluate(prefix, name, enable_planning_time_compenstation=False):
+    basename = name.split(".")[0]
+    file_plan = os.path.join(prefix, basename + "_arr_plan.txt")
+    file_act = os.path.join(prefix, basename + "_arr_act.txt")
+    file_est = os.path.join(prefix, basename + "_arr_est.txt")
+    file_result_nav = os.path.join(prefix, basename + "_result_nav.zip")
+    file_result_est = os.path.join(prefix, basename + "_result_est.zip")
+
+    # remove existing results
+    if os.path.exists(file_result_nav):
+        cmd_rm = "rm " + file_result_nav
         subprocess.call(cmd_rm, shell=True)
+    if os.path.exists(file_result_est):
+        cmd_rm = "rm " + file_result_est
+        subprocess.call(cmd_rm, shell=True)
+
+    if enable_planning_time_compenstation:
+        act = np.loadtxt(file_act)
+        plan = np.loadtxt(file_plan)
+        timeoffset = compensate_planning_time(plan[:, :4], act[:, :4])
+        if np.isnan(timeoffset):
+            raise Exception("Wrong in planning time compenstation")
+        plan[:, 0] = plan[:, 0] - timeoffset
+        file_old_plan = file_plan
+        file_plan = file_plan.replace("_arr_plan", "_arr_plan_comp")
+        np.savetxt(file_plan, plan)
+
     # run evaluation
-    cmd_eval = \
-        'evo_ape tum' + \
-        ' ' + file_gt + \
-        ' ' + file_et + \
-        ' ' + '-v --save_results ' + file_result
+    print("Evaluating navigation error ...")
+    cmd_eval = (
+        "evo_ape tum" + " " + file_plan + " " + file_act + " " + "-va --align_origin --save_results " + file_result_nav
+    )
     print(cmd_eval)
+    subprocess.call(cmd_eval, shell=True)
+
+    print("Evaluating estimation error ...")
+    cmd_eval = (
+        "evo_ape tum" + " " + file_act + " " + file_est + " " + "-va --align_origin --save_results " + file_result_est
+    )
     subprocess.call(cmd_eval, shell=True)
 
 
@@ -45,21 +104,23 @@ for f in BAGFILES:
         continue
 
     est_file = name.replace(".bag", "_arr_act.txt")
-    if os.path.exists(est_file):
-        continue
-
-    # parsing
-    print('Processing ', f)
-    cmd_parsing = \
-        'rosrun mat_from_rosbag mat_from_rosbag' + \
-        ' ' + f.as_posix() + \
-        ' ' + '/visual/odom /odom_sparse /desired_path'
-    print(cmd_parsing)
-    subprocess.call(cmd_parsing, shell=True)
+    if not os.path.exists(est_file):
+        # parsing
+        print("Processing ", f)
+        cmd_parsing = (
+            "rosrun mat_from_rosbag mat_from_rosbag"
+            + " "
+            + f.as_posix()
+            + " "
+            + "/visual/odom /odom_sparse "
+            + desired_path_topic
+        )
+        print(cmd_parsing)
+        subprocess.call(cmd_parsing, shell=True)
 
     # call evaluation
     if enable_evaluation:
-        print('Evaluating APE ...')
-        evo_evaluate(f.parent, f.name)
+        print("Evaluating APE ...")
+        evo_evaluate(f.parent, f.name, enable_planning_time_compenstation)
 
-print('Done')
+print("Done")
